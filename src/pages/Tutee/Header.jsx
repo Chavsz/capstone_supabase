84be51ea38bddec8a3ed9ee273d5b6e5fd849b2a
Orlene Bliss C. Nisnisan <orlenebliss.nisnisan@g.msuiit.edu.ph>
2025-12-15 22:52:15 +0800

    coolor

--- src/pages/Tutee/Header.jsx
+++ src/pages/Tutee/Header.jsx
@@ -378,7 +378,7 @@ const Header = () => {
   const totalNotifications = unreadCount + upcomingSessions.length;
 
   return (
-    <div className="pt-3 px-3 text-[#323335] bg-[#f4ece6]">
+    <div className="pt-3 px-3 text-[#323335] bg-[#f8f9f0]">
       <div className="flex gap-2 justify-end items-center text-[#323335] text-xl">
         {/* Notification Icon with Dropdown */}
         <div className="relative" ref={dropdownRef}>
