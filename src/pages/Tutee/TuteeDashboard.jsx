af31b7e1aba9445a2c7934e43c9b422107c6fa7d
Orlene Bliss C. Nisnisan <orlenebliss.nisnisan@g.msuiit.edu.ph>
2025-12-23 12:06:17 +0800

    session history scroll inside the container

--- src/pages/Tutee/TuteeDashboard.jsx
+++ src/pages/Tutee/TuteeDashboard.jsx
@@ -282,7 +282,7 @@ const TuteeDashboard = () => {
             </div>
 
             {/*Completed Sessions */}
-            <div className="overflow-x-auto lg:overflow-x-visible overflow-y-auto h-[280px]">
+            <div className="overflow-x-auto lg:overflow-x-visible overflow-y-auto max-h-[320px]">
               <table className="w-full text-[#1a1a1a] min-w-[640px] lg:min-w-0">
                 <thead>
                   <tr className=" border-b border-[#EBEDEF]">
